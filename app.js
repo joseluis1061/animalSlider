e396e3ca57fb8c1b5d7664e941d2b6e00402bbf1
José Luis Zúñiga <jose@tangibledesign.co>
2024-05-06 17:03:50 -0500

    Se agrega repositorio externo

---
+++ app.js
@@ -0,0 +1,58 @@
+//step 1: get DOM
+let nextDom = document.getElementById('next');
+let prevDom = document.getElementById('prev');
+
+let carouselDom = document.querySelector('.carousel');
+let SliderDom = carouselDom.querySelector('.carousel .list');
+let thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
+let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
+let timeDom = document.querySelector('.carousel .time');
+
+// Para quitar la imagen que se superpone al iniciar la animación
+// agrega el elemento de la posicion cero como primer elemento del array
+thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+
+let timeRunning = 3000; // Tiempo de espera para terminar la animación
+let timeAutoNext = 7000; // Tiempo de activación animación atomática
+
+nextDom.onclick = function(){
+  showSlider('next');    
+}
+
+// Animación automátiva
+prevDom.onclick = function(){
+  showSlider('prev');    
+}
+
+let runTimeOut;
+let runNextAuto = setTimeout(() => {
+    next.click();
+}, timeAutoNext)
+
+function showSlider(type){
+  let  SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
+  let thumbnailItemsDom = document.querySelectorAll('.carousel .thumbnail .item');
+
+  if(type === 'next'){
+    SliderDom.appendChild(SliderItemsDom[0]);
+    thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+    carouselDom.classList.add('next');
+  }else{
+    SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
+    thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
+    carouselDom.classList.add('prev');
+  }
+
+  // Eliminar la clase prev o next al terminar la animación
+  clearTimeout(runTimeOut);
+  runTimeOut = setTimeout(() => {
+    carouselDom.classList.remove('next');
+    carouselDom.classList.remove('prev');
+  }, timeRunning);
+
+  // Cambiar imagen de manera automatica
+  clearTimeout(runNextAuto);
+  runNextAuto = setTimeout(() => {
+    next.click();
+  }, timeAutoNext)
+}
